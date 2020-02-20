9d04ca65c99c65e985d17452c723816b185a4741
kelechi <oparaiskelechi@gmail.com>
2020-02-20 01:55:36 +0100

    get age functionality is done

--- app/src/main/java/com/thisiskelechi/findmyage/MainActivity.kt
+++ app/src/main/java/com/thisiskelechi/findmyage/MainActivity.kt
@@ -2,11 +2,22 @@ package com.thisiskelechi.findmyage
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+     fun displayAge(view: View) {
+        val age = dob.text.toString().toInt()
+        var calculatedAge = Calendar.getInstance().get(Calendar.YEAR) - age
+        val result = "You are $calculatedAge years old."
+         age_result.setText(result)
+        dob.setText("")
     }
 }
